74c972cd7bc7002357c636d55cd396256b454577
Christopher D'Cunha <chris.d.cunha@gmail.com>
2018-04-13 10:43:28 +0100

    HACK: Allow django-1.10 to be used.
    This is not a desirable change and should not be merged into master. The
    right thing to do is to upgrade django to a supported version.

--- setup.py
+++ setup.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-setup()
+setup(
+    name="django-oauth-toolkit",
+    version="1.1.0-hacked",
+    url="https://github.com/jazzband/django-oauth-toolkit",
+
+    author="Federico Frenguelli, Massimiliano Pippi",
+    author_email="synasius@gmail.com",
+
+    description="OAuth2 Provider for Django",
+    long_description="OAuth2 Provider for Django",
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+        "django>=1.10",
+        "oauthlib>=2.0.3",
+        "requests>=2.13.0",
+    ],
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
